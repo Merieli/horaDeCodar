f577901dd0e8f60445c6a7d832889700e2bb2c3d
Merieli Manzano <merieli.dev@gmail.com>
2022-04-13 13:27:32 -0300

    Aula 23 a 24 no minuto 4:11

--- 2_projeto_vue/src/components/Picture.vue
+++ 2_projeto_vue/src/components/Picture.vue
@@ -3,6 +3,7 @@
         <img :src="avatar" :alt="descricao">
         <!--Na chamada do componente o evento emitido deve ser chamado com @ e com sintaxe diferente do "camelcase usado na sua criação", usando palavras separadas por traço-->
         <MudarImagem @mudar-imagem="trocarImagem" />
+        <!-- Dessa forma quando o evento ocorre no componente filho é acionado o método contido no componente pai-->
     </div>
 </template>
 
@@ -22,7 +23,7 @@ export default {
     },
     methods: {
         trocarImagem() {
-
+            this.avatar = '/img/avatar2.png'
         }
     }
 }
